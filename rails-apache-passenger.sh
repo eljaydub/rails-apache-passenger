1118a9278d02d714605b021b35c7165f06ebcd32
Lee Wasilenko <lee.wasilenko@gmail.com>
2014-01-23 18:03:49 +0100

    add comments for code sections

--- rails-apache-passenger.sh
+++ rails-apache-passenger.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Check the readme for latest info
+#Check the readme and git log for latest info
 #Run the following two commands before continuing
 #sudo passwd ubuntu
 #wget --no-check-certificate https://raw.github.com/joshfng/railsready/master/railsready.sh && bash railsready.sh
@@ -9,28 +9,38 @@
 #License: MIT
 #Author: Lee Wasilenko
 
+#Update and install necessary depedencies
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install apache2 libcurl4-openssl-dev apache2-threaded-dev libapr1-dev libaprutil1-dev libwww-perl nodejs
 
+#Using ruby-1.9.3 to maintain compatibility with JRuby
 rvm install 1.9.3
 rvm use --default 1.9.3
-gem install bundler rails passenger
+gem install bundler
+#--no-ri --no-rdoc flags for rails since this takes a very long time on an t1.micro instance
+gem install --no-ri --no-rdoc rails 
+gem install passenger
 sudo dd if=/dev/zero of=/swap bs=1M count=1024
 sudo mkswap /swap
 sudo swapon /swap
 passenger-install-apache2-module
 
+#Set apache config. If the user name you install rails and ruby with 
+#is NOT ubuntu you'll have to modify the paths in the config
 sudo cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.bak
 sudo cp apache2.conf /etc/apache2/apache2.conf
 sudo chmod 644 /etc/apache2/apache2.conf
 
+#Setup your VHost and enable the site
+#Customize these to suit your app and server names
 sudo cp my-ruby-app /etc/apache2/sites-available/my-ruby-app
 sudo chmod 644 /etc/apache2/sites-available/my-ruby-app
-
 sudo a2dissite default
 sudo a2ensite my-ruby-app
 
+#Build a default Rails app in /var/www/
+#Customize this for your needs
 sudo chmod o+w /var/www/
 rails new /var/www/my-ruby-app
 cd /var/www/my-ruby-app
@@ -39,8 +49,10 @@ cd -
 sudo chmod o-w /var/www/
 sudo chown -R root:root /var/www/my-ruby-app
 
+#Restart apache
 sudo service apache2 restart
 
+#You're good to go!
 my_ec2_ip=`GET http://169.254.169.254/latest/meta-data/public-ipv4`
 echo "Go to http://$my_ec2_ip/my-ruby-app to see your default rails web app"
 echo "Logout and log back in to start using rails"
